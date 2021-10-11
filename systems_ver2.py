6f2e5ef0cab9216431fbc478a8e5e86cbb351b4f
Anastasia Lapshina <anapluslap@gmail.com>
2021-10-11 22:35:44 +0300

    added an if-statement for number == 0

--- systems_ver2.py
+++ systems_ver2.py
@@ -1,14 +1,13 @@
 def systems(number, system):
     result = []
-    while number > 0:
+    while number >= 0:
         result.append(number % system)
         number = number // system
+        if number == 0:
+            break
     result.reverse()
     final = ''.join(map(str, result))
-    if system == 2:
-        print(f"Your number in binary systems equals: {final}")
-    elif system == 8:
-        print(f"Your number in octal system equals: {final}")
+    return final
 
 
 print("Enter a number you'd like to convert: ")
@@ -24,9 +23,11 @@ try:
     system = int(input())
 except ValueError:
     print("Invalid data. Enter either 2 or 8")
-    quit()
-if (system == 2 and 0 <= number < 256) or system == 8:
-    systems(number, system)
 else:
-    print("Invalid data. Only numbers in range [0;255] can be converted into binary system")
-    quit()
+    if system == 2 and 0 <= number < 256:
+        print("Your number in binary systems equals: ", systems(number, 2))
+    elif system == 8:
+        print("Your number in octal systems equals: ", systems(number, 8))
+    elif system == 2 and (number < 0 or number > 255):
+        print("Invalid data. Only numbers in range [0;255] can be converted into binary system")
+        quit()
